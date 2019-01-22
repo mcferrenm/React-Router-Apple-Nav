16f1371ef80e4b5acf5b8403cc20ddad5d3fba70
mcferrenm <mcferrenmax@gmail.com>
2019-01-22 15:17:09 -0500

    Add Nav and SubNav component skeletons

---
+++ apple-nav/src/components/Nav.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import SubNav from "./SubNav";
+
+const Nav = () => {
+  return <SubNav />;
+};
+
+export default Nav;
--- apple-nav/src/components/NavWrapper.js
+++ apple-nav/src/components/NavWrapper.js
@@ -1,7 +1,9 @@
 import React from "react";
 
+import Nav from "./Nav";
+
 const NavWrapper = () => {
-  return <div>NavWrapper</div>;
+  return <Nav />;
 };
 
 export default NavWrapper;
