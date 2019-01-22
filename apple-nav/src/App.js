cf099bee83347075c43eff4cea0156a9540af6ef
mcferrenm <mcferrenmax@gmail.com>
2019-01-22 15:44:58 -0500

    Create mock categoryData and set to state on componentDidMount

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
+
 import NavWrapper from "./components/NavWrapper";
 
+import categoriesData from "./categoriesData";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ categories: categoriesData });
+  }
+
   render() {
     return (
       <div className="App">
---
+++ apple-nav/src/categoriesData.js
@@ -0,0 +1,321 @@
+const categoriesData = [
+  {
+    title: null,
+    img: "",
+    subCategories: []
+  },
+  {
+    title: "Mac",
+    img: "",
+    subCategories: [
+      {
+        title: "MacBook",
+        img: ""
+      },
+      {
+        title: "MacBookAir",
+        img: ""
+      },
+      {
+        title: "MacBook Pro",
+        img: ""
+      },
+      {
+        title: "iMac",
+        img: ""
+      },
+      {
+        title: "iMac Pro",
+        img: ""
+      },
+      {
+        title: "Mac Pro",
+        img: ""
+      },
+      {
+        title: "Mac mini",
+        img: ""
+      },
+      {
+        title: "Accessories",
+        img: ""
+      },
+      {
+        title: "Mojave",
+        img: ""
+      },
+      {
+        title: "Compare",
+        img: ""
+      }
+    ]
+  },
+  {
+    title: "iPad",
+    img: "",
+    subCategories: [
+      {
+        title: "iPad Pro",
+        img: "",
+        new: true
+      },
+      {
+        title: "iPad Pro",
+        img: "",
+        new: false,
+        size: '10.5"'
+      },
+      {
+        title: "iPad",
+        img: "",
+        new: false,
+        size: '9.7"'
+      },
+      {
+        title: "iPad mini 4",
+        img: "",
+        new: false,
+        size: ""
+      },
+      {
+        title: "iOS 12",
+        img: "",
+        new: false,
+        size: ""
+      },
+      {
+        title: "Apple Pencil",
+        img: "",
+        new: false,
+        size: ""
+      },
+      {
+        title: "Smart Keyboard",
+        img: "",
+        new: false,
+        size: ""
+      },
+      {
+        title: "Accessories",
+        img: "",
+        new: false,
+        size: ""
+      },
+      {
+        title: "Compare",
+        img: "",
+        new: false,
+        size: ""
+      }
+    ]
+  },
+  {
+    title: "iPhone",
+    img: "",
+    subCategories: [
+      {
+        title: "",
+        img: "",
+        new: true,
+        size: ""
+      },
+      {
+        title: "iPhone Xr",
+        img: "",
+        new: true,
+        size: ""
+      },
+      {
+        title: "iPhone 8",
+        img: "",
+        new: false,
+        size: ""
+      },
+      {
+        title: "iPhone 7",
+        img: "",
+        new: false,
+        size: ""
+      },
+      {
+        title: "iOS 12",
+        img: "",
+        new: false,
+        size: ""
+      },
+      {
+        title: "AirPods",
+        img: "",
+        new: false,
+        size: ""
+      },
+      {
+        title: "Accessories",
+        img: "",
+        new: false,
+        size: ""
+      },
+      {
+        title: "Compare",
+        img: "",
+        new: false,
+        size: ""
+      }
+    ]
+  },
+  {
+    title: "Watch",
+    img: "",
+    subCategories: [
+      {
+        title: "Apple Watch Series 4",
+        img: "",
+        new: true,
+        size: ""
+      },
+      {
+        title: "Apple Watch Nike 4+",
+        img: "",
+        new: true,
+        size: ""
+      },
+      {
+        title: "Apple Watch Hernès",
+        img: "",
+        new: TextTrackCueList,
+        size: ""
+      },
+      {
+        title: "Apple Watch Series 3",
+        img: "",
+        new: false,
+        size: ""
+      },
+      {
+        title: "watchOS",
+        img: "",
+        new: false,
+        size: ""
+      },
+      {
+        title: "Bands",
+        img: "",
+        new: false,
+        size: ""
+      },
+      {
+        title: "Accessories",
+        img: "",
+        new: false,
+        size: ""
+      },
+      {
+        title: "Compare",
+        img: "",
+        new: false,
+        size: ""
+      }
+    ]
+  },
+  {
+    title: "TV",
+    img: "",
+    subCategories: [
+      {
+        title: "Apple TV 4k",
+        img: "",
+        new: false,
+        size: ""
+      },
+      {
+        title: "Apple TV",
+        img: "",
+        new: false,
+        size: ""
+      },
+      {
+        title: "TV App",
+        img: "",
+        new: false,
+        size: ""
+      },
+      {
+        title: "Accessories",
+        img: "",
+        new: false,
+        size: ""
+      },
+      {
+        title: "Compare",
+        img: "",
+        new: false,
+        size: ""
+      }
+    ]
+  },
+  {
+    title: "Music",
+    img: "",
+    subCategories: [
+      {
+        title: "Apple Music",
+        img: "",
+        new: false,
+        size: ""
+      },
+      {
+        title: "iTunes",
+        img: "",
+        new: false,
+        size: ""
+      },
+      {
+        title: "HomePod",
+        img: "",
+        new: false,
+        size: ""
+      },
+      {
+        title: "AirPods",
+        img: "",
+        new: false,
+        size: ""
+      },
+      {
+        title: "iPodTouch",
+        img: "",
+        new: false,
+        size: ""
+      },
+      {
+        title: "Music Accessories",
+        img: "",
+        new: false,
+        size: ""
+      },
+      {
+        title: "Gift Cards",
+        img: "",
+        new: false,
+        size: ""
+      }
+    ]
+  },
+  {
+    title: "Support",
+    img: "",
+    subCategories: []
+  },
+  {
+    title: null,
+    img: "",
+    subCategories: []
+  },
+  {
+    title: null,
+    img: "",
+    subCategories: []
+  }
+];
+
+export default categoriesData;
